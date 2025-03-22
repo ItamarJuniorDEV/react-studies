7354732e5cc6990415660c55fa2489b78df43738
Itamar Junior <cdajuniorf@gmail.com>
2025-03-22 19:21:16 -0300

    feat: Implementei o retorno do componente pai.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { Button } from "./components/button"
 
 export function App() {
-  return <Button />
+  return ( 
+  <div>
+      <Button />
+      <Button />
+      <Button />
+  </div>
+  )
 }
